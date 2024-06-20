639fb547e0fe639a07cd4afe131dfbd97241c22d
lokeshmeduru123 <lokeshroyal245@gmail.com>
2024-06-20 10:01:42 +0530

    infra-project

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -15,15 +15,15 @@ variable "common_tags" {
 }
 
 variable "public_subnet_cidrs" {
-    default = ["10.0.1.0/24","10.0.2.0/24"]
+    default = ["10.0.1.0/24","10.0.2.0/24"]  #one for us-east1a & other for us-east-1b
 }
 
 variable "private_subnet_cidrs" {
-    default = ["10.0.11.0/24","10.0.12.0/24"]
+    default = ["10.0.11.0/24","10.0.12.0/24"] #one for us-east1a & other for us-east-1b
 }
 
 variable "database_subnet_cidrs" {
-    default = ["10.0.21.0/24","10.0.22.0/24"]
+    default = ["10.0.21.0/24","10.0.22.0/24"] #one for us-east1a & other for us-east-1b
 }
 
 variable "is_peering_required" {
